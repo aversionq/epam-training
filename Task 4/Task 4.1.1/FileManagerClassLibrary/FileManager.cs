80f5a36172bca2da345cc4ede6001c30fe9021ce
Arseniy K <70411996+aversionq@users.noreply.github.com>
2023-02-18 19:04:38 +0400

    Task4 (#5)
    * Done Task 3.1.1 and Task 3.1.2
    
    * Done Task 3.2.1
    
    * Done Task 3.3.1
    
    * Done Task 3.3.2
    
    * Done Task 3.3.3
    
    * Done Task 4.1.1
    
    * Improved backup method
    
    * Refactored backup methods
    
    * Fixed Task 4 after mentor's review

---
+++ Task 4/Task 4.1.1/FileManagerClassLibrary/FileManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FileManagerClassLibrary
+{
+    public class FileManager : IFileManagable
+    {
+        public string WorkPath { get; private set; }
+
+        public FileManager(string path)
+        {
+            WorkPath = path;
+        }
+
+        // Метод, запускающий файловый менеджер.
+        public void Run()
+        {
+            FileObserver fileObserver = new FileObserver(WorkPath);
+            Thread observing = new Thread(fileObserver.StartObservation);
+
+            int choice;
+
+            Console.WriteLine("Input action:" + Environment.NewLine +
+                  "1. Observation" + Environment.NewLine +
+                  "2. Stop Observation" + Environment.NewLine +
+                  "3. Backup" + Environment.NewLine +
+                  "4. Exit" + Environment.NewLine +
+                  "Input action number: " + Environment.NewLine);
+
+            do
+            {
+                string inputNum = Console.ReadLine();
+                int.TryParse(inputNum, out choice);
+                switch (choice)
+                {
+                    case 1:
+                        observing.Start();
+                        break;
+                    case 2:
+                        observing.Abort();
+                        break;
+                    case 3:
+                        FileBackupper fileBackupper = new FileBackupper(this.WorkPath, fileObserver.managerHelper.BackupDir);
+                        fileBackupper.StartBackup(this.WorkPath);
+                        break;
+                    case 4:
+                        return;
+                }
+            } while (choice > 0 && choice < 5);
+        }
+    }
+}
---
+++ Task 4/Task 4.1.1/FileManagerClassLibrary/FileObserver.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Serilog;
+
+namespace FileManagerClassLibrary
+{
+    internal class FileObserver : IObservable
+    {
+        private FileSystemWatcher watcher;  // Объект для отслеживания состояния файлов.
+        internal FileManagerHelper managerHelper = new FileManagerHelper();
+
+        public FileObserver(string dir)
+        {
+            if (!Directory.Exists(dir))
+            {
+                throw new DirectoryNotFoundException("Directory not found!");
+            }
+            else
+            {
+                managerHelper.WorkingDir = dir;
+
+                // Конфигурация отслеживателя состояния файлов.
+                watcher = new FileSystemWatcher(dir);
+                watcher.Filter = "*.txt";
+                watcher.NotifyFilter = NotifyFilters.Attributes
+                                | NotifyFilters.DirectoryName
+                                | NotifyFilters.FileName
+                                | NotifyFilters.LastAccess
+                                | NotifyFilters.LastWrite
+                                | NotifyFilters.Size;
+
+                // Создание директории с бэкапами.
+                DirectoryInfo tempDirInfo = new DirectoryInfo(dir);
+                managerHelper.BackupDir = tempDirInfo.Parent + $@"\{tempDirInfo.Name}Backup\";
+                _ = Directory.CreateDirectory(managerHelper.BackupDir);
+            }
+        }
+
+        public void StartObservation()
+        {
+            Console.WriteLine($"Started observing directory: {managerHelper.WorkingDir}");
+
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
+
+            watcher.Changed += this.OnChanged;
+            watcher.Renamed += this.OnRenamed;
+            watcher.Deleted += this.OnDeleted;
+            watcher.Created += this.OnCreated;
+        }
+
+        // Случаи изменения файлов, на которые реагирует отслеживатель файлов.
+        private async void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            Console.WriteLine($"Changed: {e.FullPath}");
+
+            await Task.Run(() => managerHelper.CreateBackup(managerHelper.WorkingDir, managerHelper.BackupDir)); // Произошло изменение - создаём бэкап.
+        }
+
+        private async void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string value = $"Created: {e.FullPath}";
+            Console.WriteLine(value);
+
+            await Task.Run(() => managerHelper.CreateBackup(managerHelper.WorkingDir, managerHelper.BackupDir)); // Произошло изменение - создаём бэкап.
+        }
+
+        private async void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Deleted: {e.FullPath}");
+
+            await Task.Run(() => managerHelper.CreateBackup(managerHelper.WorkingDir, managerHelper.BackupDir));  // Произошло изменение - создаём бэкап.
+        }
+
+        private async void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"Renamed:");
+            Console.WriteLine($"    Old: {e.OldFullPath}");
+            Console.WriteLine($"    New: {e.FullPath}");
+
+            await Task.Run(() => managerHelper.CreateBackup(managerHelper.WorkingDir, managerHelper.BackupDir));  // Произошло изменение - создаём бэкап.
+        }
+    }
+}
